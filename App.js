249bc0422d3fcc19351451a85cb194f39fcf9f96
Petr Savichev (proton) <psavichev@gmail.com>
2023-02-23 23:06:36 -0600

    add text

--- App.js
+++ App.js
@@ -2,21 +2,25 @@ import { StatusBar } from 'expo-status-bar'
 import { useState, useEffect } from 'react'
 import { StyleSheet, Text, View, PixelRatio, Button } from 'react-native'
 import { GestureHandlerRootView, TapGestureHandler, FlingGestureHandler, Directions, State } from 'react-native-gesture-handler'
+import n2words from 'n2words'
 import * as Speech from 'expo-speech'
 
 // import Button from './components/Button'
 
 export default function App() {
   const [number, setNumber] = useState(null)
+  const [numberText, setNumberText] = useState('')
 
   const changeNumber = _ => {
     const d = Math.random()
     if (d < 1/3) setNumber(Math.floor(Math.random() * 10))
     if (d < 2/3) setNumber(Math.floor(Math.random() * 100))
     else         setNumber(Math.floor(Math.random() * 1001))
+    setNumberText('')
   }
 
   const showAnswer = _ => {
+    setNumberText(n2words(number, {lang: 'es'}))
     Speech.stop()
     Speech.speak(number.toString(), { language: 'es-LA' })
   }
@@ -43,6 +47,7 @@ export default function App() {
           <TapGestureHandler onHandlerStateChange={onTap}>
             <Text style={styles.number}>{number}</Text>
           </TapGestureHandler>
+          <Text style={styles.numberText}>{numberText}</Text>
           <View style={styles.footerContainer}>
             <Button title="Show answer" onPress={showAnswer} />
             <Button title="Next" color="red" onPress={changeNumber} />
@@ -82,5 +87,10 @@ const styles = StyleSheet.create({
   number: {
     fontSize: PixelRatio.get() * 40,
     textAlign: 'center',
+  },
+  numberText: {
+    fontSize: PixelRatio.get() * 18,
+    textAlign: 'center',
+    color: 'grey',
   },
 })
