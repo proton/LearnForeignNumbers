ebdbec4c55ecc9938edc12cbc914dc241b6799b8
Petr Savichev (proton) <psavichev@gmail.com>
2023-02-26 11:34:14 -0600

    basic settings

--- components/Settings.js
+++ components/Settings.js
@@ -1,11 +1,8 @@
 import { useState, useEffect } from 'react'
-import { StyleSheet, Text, View, PixelRatio, Button, ScrollView } from 'react-native'
-// import RNPickerSelect from 'react-native-picker-select'
-// import SelectDropdown from 'react-native-select-dropdown'
-// import {Picker} from '@react-native-picker/picker'
+import { StatusBar } from 'expo-status-bar'
+import { Dimensions, StyleSheet, Text, TextInput, View, PixelRatio, Button, ScrollView, Switch, Platform } from 'react-native'
 import DropDownPicker from 'react-native-dropdown-picker'
-
-
+import { SettingsScreen } from 'react-native-settings-screen'
 
 export default function Settings() {
   // useEffect(() => {
@@ -21,9 +18,10 @@ export default function Settings() {
     { value: 'dk', label: 'Danish' },
     { value: 'de', label: 'German' },
     { value: 'en', label: 'English' },
-    { value: 'fr', label: 'French' },
     { value: 'fa', label: 'Farsi' },
+    { value: 'fr', label: 'French' },
     { value: 'he', label: 'Hebrew' },
+    { value: 'zh', label: 'Chinese' },
     { value: 'hr', label: 'Croatian' },
     { value: 'hu', label: 'Hungarian' },
     { value: 'id', label: 'Indonesian' },
@@ -41,37 +39,112 @@ export default function Settings() {
     { value: 'tr', label: 'Turkish' },
     { value: 'uk', label: 'Ukrainian' },
     { value: 'vi', label: 'Vietnamese' },
-    { value: 'zh', label: 'Chinese' },
   ]
 
   const [languageSelectOpen, setLanguageSelectOpen] = useState(false)
 
+  const settingsData = [
+    {
+      type: 'SECTION',
+      header: 'Numbers'.toUpperCase(),
+      rows: [
+        {
+          title: 'From',
+          renderAccessory: () => <TextInput
+            editable
+            onChangeText={text => console.log(text)}
+            value={0}
+            inputMode='numeric'
+          />,
+        },
+        {
+          title: 'To',
+          renderAccessory: () => <TextInput
+            editable
+            onChangeText={text => console.log(text)}
+            value={1000}
+            inputMode='numeric'
+          />,
+        },
+      ],
+    },
+    {
+      type: 'SECTION',
+      header: 'Other'.toUpperCase(),
+      rows: [
+        {
+          title: 'Immediately show the answer',
+          renderAccessory: () => <Switch
+            onValueChange={text => console.log(text)}
+            value={false}
+          />,
+        },
+        {
+          title: 'Languge',
+          subtitle: 'Subtitle',
+          showDisclosureIndicator: true,
+        },
+        {
+          title: 'Text',
+          renderAccessory: () => (
+            <DropDownPicker
+              open={languageSelectOpen}
+              value={language}
+              items={languages}
+              setOpen={setLanguageSelectOpen}
+              setValue={setLanguage}
+            />
+          ),
+        },
+      ],
+    },
+    {
+      type: 'SECTION',
+      rows: [
+        {
+          renderAccessory: () => (
+            <Button title='Start' color='red'></Button>
+          ),
+        },
+      ],
+    },
+  ]
+
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <Text>Language:</Text>
-
-      <DropDownPicker
-        open={languageSelectOpen}
-        value={language}
-        items={languages}
-        setOpen={setLanguageSelectOpen}
-        setValue={setLanguage}
+    <View style={styles.container}>
+      <View style={styles.navBar}>
+        <Text style={styles.navBarTitle}>Settings</Text>
+      </View>
+      <SettingsScreen
+        data={settingsData}
       />
-
-      <Text>Other:</Text>
-
-      <Button title='Start' color='red'></Button>
-    </ScrollView>
+    </View>
   )
 }
+
+const fontFamily = Platform.OS === 'ios' ? 'Avenir' : 'sans-serif'
+
+const statusBarHeight = Platform.OS === 'ios' ? 35 : 35
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: 'green',
     flexShrink: 0,
     flexGrow: 1,
+  },
+  navBar: {
+    backgroundColor: '#8c231c',
+    height: 44 + statusBarHeight,
+    alignSelf: 'stretch',
+    paddingTop: statusBarHeight,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  navBarTitle: {
+    color: 'white',
+    fontFamily,
+    fontSize: 17,
   },
 })
