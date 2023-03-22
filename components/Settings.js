48a0279c0e9393ce5189502ec466c9536fa850ee
Petr Savichev (proton) <psavichev@gmail.com>
2023-03-21 18:21:38 -0600

    fix setting default language

--- components/Settings.js
+++ components/Settings.js
@@ -39,7 +39,7 @@ export default function Settings(props) {
   }
 
   useEffect(_ => {
-    if (!prefs.voice) changeLanguage()
+    if (!prefs.voice) changeLanguage(prefs.language)
   })
 
   const startGame = _ => EventBus.emit('closeSettings')
