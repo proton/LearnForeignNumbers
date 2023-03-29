cfb11416f1d009dd6ae05b9cb55ae45e400b0433
Petr Savichev (proton) <psavichev@gmail.com>
2023-03-28 20:49:40 -0600

    fix sound in ios

--- components/Player.js
+++ components/Player.js
@@ -1,16 +1,30 @@
-import n2words     from 'n2words'
-import * as Speech from 'expo-speech'
+import n2words      from 'n2words'
+import * as Speech  from 'expo-speech'
+import { Platform } from 'react-native'
+import { Audio }    from 'expo-av'
 
 export default class Player {
   constructor({ voice, language }) {
     this.voice = voice
     this.language = language
+    this.enableSound()
   }
 
-  sayNumber(number) {
+  async enableSound() {
+    if (Platform.OS === 'ios') {
+      await Audio.setAudioModeAsync({
+        playsInSilentModeIOS: true,
+      })
+      const soundObject = new Audio.Sound()
+      await soundObject.loadAsync(require('../assets/empty.mp3'))
+      await soundObject.playAsync()
+    }
+  }
+
+  async sayNumber(number) {
     if (!this.voicePresent()) return
     const text = this.numberToText(number)
-    Speech.stop()
+    if (await Speech.isSpeakingAsync()) Speech.stop()
     Speech.speak(text, { language: this.language, voice: this.voice })
   }
 
