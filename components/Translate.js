ea9099fa3cee4bd88b43a1092a83e62179f45f3c
Petr Savichev (proton) <psavichev@gmail.com>
2023-04-05 07:25:30 -0600

    fix translations load

--- components/Translate.js
+++ components/Translate.js
@@ -1,10 +1,14 @@
-import { I18n }    from 'i18n-js'
-import * as Consts from './Constants'
+import { I18n } from 'i18n-js'
+import de       from '../translations/de.json'
+import en       from '../translations/en.json'
+import es       from '../translations/es.json'
+import it       from '../translations/it.json'
+import ru       from '../translations/ru.json'
+import uk       from '../translations/uk.json'
+import zh_CN    from '../translations/zh_CN.json'
+import zh_TW    from '../translations/zh_TW.json'
 
-const translations = {}
-for (const locale of Consts.LOCALES) {
-  translations[locale] = require(`../translations/${locale}.json`)
-}
+const translations = { de, en, es, it, ru, uk, zh_CN, zh_TW }
 
 export default function Translate(locale) {
   const i18n = new I18n(translations)
